50f69db90772381cb18d6eb842b06533ba587443
cjmakes <cj@cjmakes.com>
2025-03-04 18:05:01 -0800

    Adds flake devshell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go
+          gopls
+          gnumake
+          rrdtool
+          unixtools.ping
+        ];
+      };
+    };
+}
